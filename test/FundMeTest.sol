d22c8f56deabfcc2387ed7fd61c861ee281bd15d
Oliver Gjorgjiev <oliver.gorgiev11@gmail.com>
2024-12-22 22:53:29 +0100

    tests

---
+++ test/FundMeTest.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+
+contract FundMeTest is Test {
+    uint256 number = 1;
+    function setUp() external {
+        number = 2;
+    }
+
+    function testDemo() public {
+        assertEq(number, 2);
+    }
+}
